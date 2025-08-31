b434c3445e8af2de8179385d76ac6b440d35a042
Jeremy Zabala <jeremy.zbala@gmail.com>
2025-08-31 09:27:27 -0600

    chore: public files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
-}
+  title: "Biznes | El asistente de tu negocio",
+  description:
+    "El asistente que tu negocio necesitaba, punto de venta, inventario y más.",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
     </html>
-  )
+  );
 }
